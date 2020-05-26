bf96b82fba0690bcf5538b801eb1bd77a2b1c4c9
HY <laurahy172@gmail.com>
2020-05-26 23:05:26 +0800

    first try push

---
+++ proj0/NBody.java
@@ -0,0 +1,113 @@
+public class NBody {
+    /** Class to run the simulation, have NO constructor 
+     * Goal is to simulate a universe specified in one of the data file
+    */
+
+    // give a filename return a double of the radius of the universe in that file
+    public static double readRadius(String filename) {
+        In in = new In(filename);
+        int N = in.readInt();
+        double radius = in.readDouble();
+        return radius;
+    }
+    
+    // returns an array of body
+    public static Planet[] readPlanets(String filename) {
+        In in = new In(filename);
+        int N = in.readInt();
+        double radius = in.readDouble();
+        Planet[] return_bodies = new Planet[N]; // need two new, one is to instaitate arrays; one is for body
+
+        for (int i = 0; i < N; i++) {
+            double x_pos = in.readDouble();
+            double y_pos = in.readDouble();
+            double x_vel = in.readDouble();
+            double y_vel = in.readDouble();
+            double mass = in.readDouble();
+            String img_name = in.readString();
+
+            return_bodies[i] = new Planet(x_pos, y_pos, x_vel, y_vel, mass, img_name);
+        }
+        return return_bodies;
+    }
+
+    public static void main(String[] args) {
+        double T = Double.parseDouble(args[0]); //  ## convert a string to a double format
+        double dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+        Planet[] body_list = readPlanets(filename);
+        double radius = readRadius(filename);
+
+        //prevent flicking in the animation;
+        StdDraw.enableDoubleBuffering();
+
+        // set canvas
+        StdDraw.setXscale(-radius, radius); // (0,0) is in the center, radius is the half of the canvas
+        StdDraw.setYscale(-radius, radius);
+
+        // declare and instantiate two arrays of Body object
+        double[] xForces = new double[body_list.length];
+        double[] yForces = new double[body_list.length];
+
+        // for simulation from time 0 to T
+        for (double t = 0; t < T; t = t + dt) {
+
+            // get the forces in x and y dimention
+            for (int i = 0; i < body_list.length; i++) {
+                xForces[i] = body_list[i].calcNetForceExertedByX(body_list);
+                yForces[i] = body_list[i].calcNetForceExertedByY(body_list);
+            }
+
+            // update the postion of body in the array
+            for (int i = 0; i < body_list.length; i++) {
+                body_list[i].update(dt, xForces[i], yForces[i]);
+            }
+
+            // draw the background
+            StdDraw.picture(0, 0, "images/starfield.jpg");
+
+            // draw each body in the array 
+            for (int i = 0; i < body_list.length; i++) {
+                body_list[i].draw();
+            }
+
+            StdDraw.show();
+            StdDraw.pause(10);
+
+        }
+
+        // final print
+        // System.out.println(body_list.length);
+        // System.out.println(radius);
+        // for (Body b:body_list) {
+        //     System.out.print(b.xxPos + ' ');
+        //     System.out.print(b.yyPos + ' ');
+        //     System.out.print(b.xxVel + ' ');
+        //     System.out.print(b.yyVel + ' ');
+        //     System.out.print(b.mass + ' ');
+        //     System.out.println(b.imgFileName);
+        // }
+
+        // ## https://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax
+        // ## formatter
+        // final print
+        StdOut.printf("%d\n", body_list.length);
+        StdOut.printf("%.2e\n", radius); // precions 2 after the floating point
+        for (Planet b : body_list) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s \n ",
+            b.xxPos,b.yyPos,b.xxVel,b.yyVel,b.mass,b.imgFileName);
+        }
+
+
+
+
+    }
+}
+
+
+/**
+ * java NBody 157788000.0 25000.0 data/planets.txt
+ * javac NBody.java
+ * 
+ *  
+ * */
---
+++ proj0/Planet.java
@@ -0,0 +1,100 @@
+public class Planet {
+    // adding instance variable;
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    // adding Body constructor; later can represent a planet, star or various objects in this universe;
+    public Planet(double xP, double yP, double xV, double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+    
+    // adding constructor that copy the body object
+    public Planet(Planet b) {
+        xxPos = b.xxPos;
+        yyPos = b.yyPos;
+        xxVel = b.xxVel;
+        yyVel = b.yyVel;
+        mass = b.mass;
+        imgFileName = b.imgFileName;
+    }
+
+    // add instance method
+    // ## use Math.pow() and Math.sqrt()
+    public double calcDistance(Planet b) {
+
+        double r_square = Math.pow(b.xxPos - this.xxPos, 2) + Math.pow(b.yyPos - this.yyPos, 2);
+        return Math.sqrt(r_square);
+    }
+    
+    // ## decalre a constant that will be used in the class;
+    // ## scienctific 10^-11
+    final static double G = 6.67e-11;
+    
+    public double calcForceExertedBy(Planet b) {
+        double r = this.calcDistance(b);
+        double force = G * this.mass * b.mass / r / r;
+        return force;
+    }
+
+    public double calcForceExertedByX(Planet b) {
+        double dx = b.xxPos - this.xxPos;
+        return this.calcForceExertedBy(b) * dx / this.calcDistance(b);
+
+    }
+    
+    public double calcForceExertedByY(Planet b) {
+        double dy = b.yyPos - this.yyPos;
+        return this.calcForceExertedBy(b) * dy / this.calcDistance(b);
+    }
+    
+    //Write methods calcNetForceExertedByX and calcNetForceExertedByY that each take in an array of Bodys and calculates the net X and net Y force exerted by all bodies in that array upon the current Body. For example, consider the code snippet below:
+    // ## To compare two bodies, use the .equals method instead of ==: samh.equals(samh) (which would return true)
+    public double calcNetForceExertedByX(Planet[] bodies) {
+        double net_force_x = 0;
+        for (int i = 0; i < bodies.length; i++) {
+            if (bodies[i].equals(this)) {
+                continue;}
+
+            net_force_x += this.calcForceExertedByX(bodies[i]);
+        }
+        return net_force_x;
+    }
+
+    public double calcNetForceExertedByY(Planet[] bodies) {
+        double net_force_y = 0;
+        for (int i = 0; i < bodies.length; i++) {
+            if (bodies[i].equals(this)) {
+                continue;
+            }
+            net_force_y += this.calcForceExertedByY(bodies[i]);
+        }
+        return net_force_y;
+    }
+    
+    // write a method update the velocity,acceleration, and poistion of the object, no returns
+    public void update(double dt, double Fx, double Fy) {
+        double a_x = Fx / this.mass;
+        double a_y = Fy / this.mass;
+
+        this.xxVel = this.xxVel + dt * a_x;
+        this.yyVel = this.yyVel + dt * a_y;
+
+        this.xxPos = this.xxPos + this.xxVel * dt;
+        this.yyPos = this.yyPos + this.yyVel * dt;
+    }
+
+    public void draw() {
+        StdDraw.picture(this.xxPos, this.yyPos,"images/"+this.imgFileName);
+    }
+    
+    
+}
